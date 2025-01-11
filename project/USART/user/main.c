c054d94a54fe9dba5e6f13b47bf6378d3dfcd4a7
xyz <2366206118@qq.com>
2025-01-11 22:34:14 +0800

    打印字符串，输出时间

--- project/USART/user/main.c
+++ project/USART/user/main.c
@@ -1,4 +1,6 @@
 #include "stm32f10x.h"
+#include <stdio.h>//FILE
+#include "delay.h"//GetTick
 
 /*
 void USART_Init(串口名称，初始化参数)
@@ -17,21 +19,80 @@ struct USART_InitTypeDef//初始化串口
 	                      //USART_Parity_No: 无校验
 												//USART_Parity_Even: 偶校验
 												//USART_Parity_Odd: 奇校验
-	unit13_t USART_Mode;//数据收发方向
+	uint13_t USART_Mode;//数据收发方向
 	                   //USART_Mode_Tx: 发数据
 										 //USART_Mode_Rx: 收数据
 										 //USART_Mode_Tx | USART_Mode_Rx: 收发双向
 }
 */
 
+/*
+FlagStatus USART_GetFlagStatus(串口名称，查询的标志位)
+查询USART的标志位的返回值，RESET=0,SET=1
+*/
+
+/*
+void USART_SendData(串口名称，要发送的数据（无符号16bit位整型，2个字节）)
+把要发送的数据写入发送数据寄存器中
+*/
+
 void My_USART_SendBytes(USART_TypeDef *USARTx, uint8_t *pData, uint16_t Size);
 //定义一个函数，使用串口一次性发送多个字节
-//参数：串口名称，要发送的数据（数组），发送字节数量 
+//参数：串口名称，要发送的数据（数组），发送字节数量
+
+void My_USART1__Init(void);//定义初始化函数，初始化IO引脚和USART1串口
 
 int main(void)
 {
-	//	//配置USART1引脚，默认在PA9,PA10,REMAP=0
-//	//USART1_Tx  PA9,复用推挽模式
+	Delay_Init();//调用函数，获取当前时间
+	
+	My_USART1__Init();//调用函数，初始化
+	
+//	//测试
+//	uint8_t bytesSend[] = {1,2,3,4,5};//发送的数据
+//	My_USART_SendBytes(USART1, bytesSend, 5);//根据Size输出多少
+	
+//	printf("hello world\r\n");
+	// \r\n表示换行回车
+	
+	while(1)
+	{
+		uint32_t currentTick = GetTick();//获取当前时间，得到一个数字
+		uint32_t miliSeconds = currentTick % 1000;//计算毫秒
+		currentTick /= 1000;
+		uint32_t seconds = currentTick % 60;//计算秒
+		currentTick /= 60;
+		uint32_t minutes = currentTick % 60;//计算分钟
+		currentTick /= 60;
+		uint32_t hours = currentTick;//计算小时
+		printf("%02u:%02u:%02u.%03u\r\n",hours,minutes,seconds,miliSeconds);
+		//%u无符号整型；%02u,2位不足用0补；%2u，2位不足用空格补
+		
+		Delay(100);//每100ms打印一次
+	}
+}
+
+void My_USART_SendBytes(USART_TypeDef *USARTx, uint8_t *pData, uint16_t Size)
+{
+	for(uint32_t i = 0; i < Size; i++)
+	{
+		//1.等待发送数据寄存器为空
+		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+		//查询TXE标志位，RESET=0,TXE=0,非空，一直循环，当变为SET=1时，空，进行下一步程序
+		
+		//2.要发送的数据写入发送数据寄存器中
+		USART_SendData(USART1, pData[i]);
+	}
+	
+	//3.等待数据发送完成
+	while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+	//查询TC标志位，TC=0=RESET,未完成；TC=1=SET,完成	
+}
+
+void My_USART1__Init(void)
+{
+//	//配置USART1引脚，默认在PA9,PA10,REMAP=0
+//	//初始化USART1_Tx  PA9,复用推挽模式
 //	GPIO_InitTypeDef GPIO_InitStruct;//定义PA9的IO引脚结构体指针
 //	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);//开启GPIOA时钟
 //	GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9;//引脚9
@@ -39,7 +100,7 @@ int main(void)
 //	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_10MHz;//最大输出速度10MHz
 //	GPIO_Init(GPIOA, &GPIO_InitStruct);//初始化完成PA9
 //	
-//	//USART_Rx  PA10,输入上拉模式
+//	//初始化USART1_Rx  PA10,输入上拉模式
 //	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);//开启GPIOA时钟
 //	GPIO_InitTypeDef GPIO_InitStruct1;//定义PA10的引脚结构体
 //	GPIO_InitStruct1.GPIO_Pin = GPIO_Pin_10;
@@ -65,6 +126,7 @@ int main(void)
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_Init(GPIOB, &GPIO_InitStruct);//初始化完成PB7引脚
 	
+	//串口初始化
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);//开启USART1时钟
 	USART_InitTypeDef USART_InitStruct;//定义串口USART结构体指针
 	USART_InitStruct.USART_BaudRate = 115200;
@@ -76,40 +138,16 @@ int main(void)
 	
 	USART_Cmd(USART1, ENABLE);//USART模块使能
 	                //DISABLE,禁止
-
-	uint8_t bytesSend[] = {1,2,3,4,5};//发送的数据
-	My_USART_SendBytes(USART1, bytesSend, 5);//根据Size输出多少
-
-/*
-FlagStatus USART_GetFlagStatus(串口名称，查询的标志位)
-查询USART的标志位的返回值，RESET=0,SET=1
-*/
-
-/*
-void USART_SendData(串口名称，要发送的数据（无符号16bit位整型，2个字节）)
-把要发送的数据写入发送数据寄存器中
-*/
-
-	while(1)
-	{
-	}
 }
 
-void My_USART_SendBytes(USART_TypeDef *USARTx, uint8_t *pData, uint16_t Size)
+int fputc(int ch, FILE *f)//重定义fputc，通过串口发送数据
 {
-	for(uint32_t i = 0; i < Size; i++)
-	{
-		//1.等待发送数据寄存器为空
-		while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-		//查询TXE标志位，RESET=0,TXE=0,非空，一直循环，当变为SET=1时，空，进行下一步程序
-		
-		//2.要发送的数据写入发送数据寄存器中
-		USART_SendData(USART1, pData[i]);
-	}
+	//1.等发送数据寄存器为空
+	while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
 	
-	//3.等待数据发送完成
-	while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
-	//查询TC标志位，TC=0=RESET,未完成；TC=1=SET,完成	
+	//2.要发送的数据写入发送数据寄存器中
+	USART_SendData(USART1, (uint8_t)ch);
+	                      //强制转换成字节
+	
+	return ch;
 }
-
-
